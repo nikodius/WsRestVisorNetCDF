e0b55074fcc110ceef501377ff8ae8aec2084817
Nicolas Rodriguez <nicolasrg2502@gmail.com>
2020-11-17 21:07:25 -0500

    se agrega crossOrigin, documentacion

--- src/main/java/com/visorNetCDF/controlador/NetCDFController.java
+++ src/main/java/com/visorNetCDF/controlador/NetCDFController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,7 @@ import com.visorNetCDF.persistencia.VariableDTO;
  * clase controlador con peticiones del servicio netCDF
  *
  */
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/netCDF")
 public class NetCDFController {
